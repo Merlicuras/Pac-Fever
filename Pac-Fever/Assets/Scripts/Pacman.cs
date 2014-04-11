442e1b436d37e1b776cb8de54ba9ae473aba95e5
kristoz666 <kristoz666@hotmail.com>
2014-04-11 14:45:04 +0200

    Pacman script added

---
+++ Pac-Fever/Assets/Scripts/Pacman.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacman : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		public float speed = 20.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = transform.position + new Vector3(0,  0,  speed) * Time.deltaTime;
+		
+		if(Input.GetKeyDown(KeyCode.A))
+			transform.position += new Vector3(-2.0f, 0, 0);
+		
+		if(Input.GetKeyDown(KeyCode.D))
+			transform.position += new Vector3(2.0f, 0, 0);
+		
+		if (Input.GetKeyDown(KeyCode.W))
+			transform.position += new Vector3(0, 2.0f, 0);
+		if(Input.GetKeyDown(KeyCode.S))
+			transform.position += new Vector3(0, -2.0f, 0);
+
+	}
+}
