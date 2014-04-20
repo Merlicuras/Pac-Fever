79593b7d3280615bfb505857684489c0ec6aa939
kristoz666 <kristoz666@hotmail.com>
2014-04-20 14:20:39 +0200

    Collison detection for Pacman
    I still don’t know how to the ubercheese last for 10 secs. I also think
    we need a script for the ghosts.

--- Pac-Fever/Assets/Scripts/Pacman.cs
+++ Pac-Fever/Assets/Scripts/Pacman.cs
@@ -5,18 +5,58 @@ public class Pacman : MovingObject {
 
 
 
+	private int lives;
+
+	public GUIText winText;
+	public GUIText livesText;
+
+	SetLivesText ();
+	winText.text = "";
+
+	private bool state;
+
+	public bool ghosts; //maybe this could be put in the ghosts' scripts to make them vulnerable?
+
+
+
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.name == "ghost"){
+			Destroy(this.gameObject);
+			Debug.Log ("collision");
+			lives=lives-1;
+			SetLivesText ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.name == "UberCheese"){
+			state = true;
+			Debug.Log ("collision");
+		}
+	}
+	
 	// Use this for initialization
 	void Start () {
-
-
+		lives = 3;
+		state = false;
 
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
 
+		void SetLivesText (){
+			livesText.text = "Lives: " + lives.ToString();
+			if (lives <= 0){
+			winText.text = "GAME OVER";
+			}
+		}
 
-		
+		if (state = true;)
+			ghosts = true; //true = vulnerable
+
+
 		if (Input.GetKeyDown (KeyCode.A))
 			base.left();
 		
