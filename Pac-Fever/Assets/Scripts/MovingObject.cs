2ef6306611b714246b5e0fa7c2a2d4951c8bda25
Father Grigori <grigori@Ravenholm>
2014-04-28 20:15:29 +0200

    Added bool 'isMoving' for (de)activation of movement (currently defaulted to true)

--- Pac-Fever/Assets/Scripts/MovingObject.cs
+++ Pac-Fever/Assets/Scripts/MovingObject.cs
@@ -6,6 +6,7 @@ public class MovingObject : MonoBehaviour {
 
 	public float speed = 0.1f;
 	public Vector3 direction;
+	public bool isMoving;
 
 	public void right ()
 	{
@@ -27,7 +28,8 @@ public class MovingObject : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-		
+		isMoving = true;	//Should be false, when GameManager can 
+						// handle activation of Pacman & Control
 		
 	}
 	
@@ -37,8 +39,8 @@ public class MovingObject : MonoBehaviour {
 		//transform.position = transform.position + new Vector3(0,  0,  speed) * Time.deltaTime;
 		
 
-		
-		transform.Translate (direction * speed);
+		if(isMoving)
+			transform.Translate (direction * speed);
 
 	}
 
